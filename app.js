9017d0dc4c8da05eacbc42601ef0863fb6942828
Otavio Kappann <kappannotavio@gmail.com>
2025-08-05 11:31:30 -0300

    Primeiro Commit

---
+++ app.js
@@ -0,0 +1,32 @@
+let listaAmigos = [];
+
+function adicionarAmigo(){
+    var amigo = document.getElementById("amigo").value;
+
+    if(amigo){
+    listaAmigos.push(amigo);
+    atualizarLista(amigo);
+    }
+    else{
+        alert("Nome do amigo não pode estar vazio!");
+    }
+}
+
+function atualizarLista(amigo){
+    var lista  = document.getElementById("listaAmigos").innerHTML;
+    lista = lista +"<li>"+amigo+"</li>";
+    document.getElementById("listaAmigos").innerHTML = lista;
+}
+
+function sortearAmigo(){
+    var i = parseInt(Math.random() * listaAmigos.length);
+    console.log(i);
+    mostrarVencedor(listaAmigos[i]);
+}
+
+function mostrarVencedor(vencedor){
+    document.getElementById("resultado").innerHTML = "O vencedor foi o " + vencedor;
+    console.log(vencedor);
+}
+
+
